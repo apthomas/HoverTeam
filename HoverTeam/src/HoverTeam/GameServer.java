12eff8f4bc8dc5079a1d1b919faa1317aaf8f627
Matt Vernacchia <mvernacc@mit.edu>
2014-05-04 14:33:12 -0400

    file structure and blank classes

---
+++ HoverTeam/src/HoverTeam/GameServer.java
@@ -0,0 +1,10 @@
+/**
+ * HoverTeam
+ * 16.35 Spring 2014 Final Project
+ * @author Aaron Thomas and Matt Vernacchia
+ */
+package HoverTeam;
+
+public class GameServer {
+
+}
---
+++ HoverTeam/src/HoverTeam/Physics.java
@@ -0,0 +1,14 @@
+/**
+ * HoverTeam
+ * 16.35 Spring 2014 Final Project
+ * @author Aaron Thomas and Matt Vernacchia
+ */
+package HoverTeam;
+
+/**
+ * @author mvernacc
+ *
+ */
+public class Physics {
+
+}
