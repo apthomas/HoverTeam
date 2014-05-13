c393089ce0ce8561fd793d3082604e92e8ae087f
Matt Vernacchia <mvernacc@mit.edu>
2014-05-13 16:34:05 -0400

    game state reception in GameClient

--- HoverTeam/src/HoverTeam/GameClient.java
+++ HoverTeam/src/HoverTeam/GameClient.java
@@ -5,6 +5,89 @@
  */
 package HoverTeam;
 
-public class GameClient {
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class GameClient implements Runnable {
+	private GameState state;
+
+	public GameClient() {
+
+	}
+	/**
+	 * Convert a serialized data array into an Object.
+	 * The GameServer broadcasts the game state data to the GameClient over UDP.
+	 * This method is used to decode that data into a GameState Object.
+	 * @param data
+	 * @return The data converted into a java Object.
+	 */
+	public static Object deserialize(byte[] data){
+		ByteArrayInputStream in = new ByteArrayInputStream(data);
+		ObjectInputStream is;
+		try {
+			is = new ObjectInputStream(in);
+			return is.readObject();
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public synchronized void setState(GameState state) {
+		if(state != null) {
+			this.state = state;
+		}
+	}
+
+	public synchronized GameState getState() {
+		return this.state;
+
+	}
+
+	public static void main(String[] argv) {
+		GameClient gc = new GameClient();
+		GameClientReceiver gcr = new GameClientReceiver(gc);
+		// Start the Client-side threads
+		Thread gc_thread = new Thread(gc);
+		Thread gcr_thread = new Thread(gcr);
+		gc_thread.start();
+		gcr_thread.start();
+		// Wait for the GameClient thread to finish
+		try {
+			gc_thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		// Kill the GameClientReceiver
+		System.out.println("Client shutting down...");
+		gcr.on = false;
+		try {
+			gcr_thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		while(true) {
+			GameState state = getState();
+			if(state != null) {
+				if(!state.getGameOutcome()) {break;}
+				System.out.println(state.getTime());
+			}
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
+
---
+++ HoverTeam/src/HoverTeam/GameClientReceiver.java
@@ -0,0 +1,44 @@
+package HoverTeam;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class GameClientReceiver implements Runnable {
+	public boolean on = true;
+	private GameClient gc;
+
+	public GameClientReceiver(GameClient gc) {
+		this.gc = gc;
+	}
+	public void run() {
+		try{
+			MulticastSocket socket = new MulticastSocket(GameServer.multicast_port_rcv);
+			InetAddress group = InetAddress.getByName(GameServer.multicast_ip_addr);
+			socket.joinGroup(group);
+			System.out.println("Client up");
+			DatagramPacket packet;
+			while(on) {
+				byte[] buf = new byte[1024];
+				// Receive a packet
+				packet = new DatagramPacket(buf, buf.length);
+				socket.receive(packet);
+				// Get the data out of the packet
+				byte[] data = packet.getData();
+				// Reconstruct the data into a GameState
+				GameState state = (GameState) GameClient.deserialize(data);
+				// Set the GameClient's state
+				this.gc.setState(state);
+				//System.out.println(state.getTime());
+			}
+
+			socket.leaveGroup(group);
+			socket.close();
+			System.out.println("Client socket closed");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
