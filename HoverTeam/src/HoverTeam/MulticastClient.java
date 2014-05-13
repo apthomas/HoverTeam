4038f61b5cf1372e6f6a66ec6b43b22f59fa18e6
Matt Vernacchia <mvernacc@mit.edu>
2014-05-12 23:34:46 -0400

    working object serialization and transmission via UDP

--- HoverTeam/src/HoverTeam/MulticastClient.java
+++ HoverTeam/src/HoverTeam/MulticastClient.java
@@ -1,11 +1,25 @@
 package HoverTeam;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
 public class MulticastClient {
+	
+    public static Object deserialize(byte[] data){
+    	ByteArrayInputStream in = new ByteArrayInputStream(data);
+        ObjectInputStream is;
+        try {
+        	is = new ObjectInputStream(in);
+			return is.readObject();
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+        return null;
+    }
 	public static void main(String args[]) {
 		try{
 			MulticastSocket socket = new MulticastSocket(4446);
@@ -14,11 +28,15 @@ public class MulticastClient {
 			System.out.println("Client up");
 			DatagramPacket packet;
 			for (int i = 0; i < 10; i++) {
-				byte[] buf = new byte[256];
+				byte[] buf = new byte[1024];
+				// Receive a packet
 				packet = new DatagramPacket(buf, buf.length);
 				socket.receive(packet);
-				String received = new String(packet.getData());
-				System.out.println(received);
+				// Get the data out of the packet
+				byte[] data = packet.getData();
+				// Reconstruct the data into a GameState
+				GameState state = (GameState) deserialize(data);
+				System.out.println(state.getTime());
 			}
 
 			socket.leaveGroup(group);
--- HoverTeam/src/HoverTeam/MulticastServer.java
+++ HoverTeam/src/HoverTeam/MulticastServer.java
@@ -11,52 +11,62 @@ import java.net.UnknownHostException;
 
 public class MulticastServer implements Runnable{
 	DatagramSocket socket;
-    ByteArrayOutputStream byteStream;
+	ByteArrayOutputStream byteStream;
 
-    ObjectOutputStream objectStream;
+	ObjectOutputStream objectStream;
 
 	public MulticastServer() {
-		byteStream = new ByteArrayOutputStream();
-		try {
-			objectStream = new ObjectOutputStream(byteStream);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
+		
 		try {
 			socket = new DatagramSocket(4445);
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}		
 	}
-	
+
 	public byte[] serialize(Object o) {
+		byteStream = new ByteArrayOutputStream();
 		try {
+			objectStream = new ObjectOutputStream(byteStream);
 			objectStream.writeObject(o);
+			objectStream.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	    return byteStream.toByteArray();
+		return byteStream.toByteArray();
 	}
 
 	@Override
 	public void run() {
+		// Set up the group to multicast to
+		InetAddress group = null;
+		try {
+			group = InetAddress.getByName("224.0.0.1");
+		} catch (UnknownHostException e2) {
+			e2.printStackTrace();
+		}
 		System.out.println("Server up");
+		// Make a new GameState to send
+		int[] nearObstHeights = {1};
+		int nearObstIndex = 0;
+		double[] pos = {5, 5, 0};
+		double[] vel = {0, 0, 0};
+		GameState state = new GameState(pos, vel, 0, 0, nearObstHeights, nearObstIndex);
+
 		for(int i=0; i < 10; i++){
-			byte[] buf = new byte[256];
-			String dString = String.format("Message %d", i);
-			buf = dString.getBytes();
-			InetAddress group;
+			// Log the cycle count in the GameState time
+			double time = 1.0*i;
+			System.out.println(time);
+			state.setTime(time);
+			// Convert the GameState into a byte array
+			byte[] data = serialize(state);
+			// Pack the byte array into a packet
+			DatagramPacket packet;
+			packet = new DatagramPacket(data, data.length, group, 4446);
+			// Send the packet
 			try {
-				group = InetAddress.getByName("224.0.0.1");
-				DatagramPacket packet;
-				packet = new DatagramPacket(buf, buf.length, group, 4446);
-				try {
-					socket.send(packet);
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-			} catch (UnknownHostException e1) {
-				// TODO Auto-generated catch block
+				socket.send(packet);
+			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
 			try {
@@ -66,7 +76,7 @@ public class MulticastServer implements Runnable{
 		}
 		socket.close();
 	}
-	
+
 	public static void main(String args[]) {
 		MulticastServer server = new MulticastServer();
 		Thread server_thread = new Thread(server);
